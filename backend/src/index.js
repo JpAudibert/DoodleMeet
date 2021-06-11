13738aa4b54b2b3b5deaac0c8e168d317d7b66e6
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-06-11 17:42:22 -0300

    solving heroku problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
-const app = require('express')();
-const server = require('http').createServer(app);
+const express = require('express');
 const cors = require('cors');
 
+const app = express();
+
+const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
